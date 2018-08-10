ee227c87ce7132c734d8f3daa2af5952354d1364
tejeshatr <teammarvel0@gmail.com>
2018-08-10 20:15:52 +0530

    Done with pig_latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,25 @@
-#write your code here
+def tr(w)
+	vowels = 'aeiouAEIOU'
+	i = 0
+	while !(vowels.include? w[i])
+		if w[i..i+1] == 'qu'
+			i += 1
+		end
+		i += 1
+	end
+	p i
+	if i != 0
+		w[i..-1]+w[0..i-1]+'ay'
+	else
+		w+'ay'
+	end
+end
+
+
+def translate(str)
+	r = str.split(" ")
+	r.length.times do |i|
+		r[i] = tr(r[i])
+	end
+	r.join(" ")
+end
