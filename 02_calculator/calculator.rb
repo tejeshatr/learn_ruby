86902e5f39fe0ec4da08ddb10e918c8cd72bbe0c
tejeshatr <teammarvel0@gmail.com>
2018-08-10 19:26:24 +0530

    Done with calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,32 @@
-#write your code here
+def add(x, y)
+	x+y
+end
+
+def subtract(x, y)
+	x-y
+end
+
+def multiply(x, y)
+	x*y
+end
+
+def sum(arr)
+	s = 0
+	arr.length.times do |i|
+		s += arr[i]
+	end
+	s
+end
+
+def power(x, y)
+	(y-1).times do
+		x *= y
+	end
+end
+
+def factorial(x)
+	if x == 1
+		return 1
+	end
+	x*factorial(x-1)
+end
