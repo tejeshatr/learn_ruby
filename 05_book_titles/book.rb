6889b1ae6b0b96455aced6e5684c815cab55366d
tejeshatr <teammarvel0@gmail.com>
2018-08-10 21:14:35 +0530

    Done with book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,23 @@
 class Book
-# write your code here
+	attr_reader :title
+
+	def title=(str)
+		littles = ['a', 'an', 'the', 'and', 'or', 'in', 'on', 'of']
+		res = str.split(" ")
+		x = ''
+		(res.length).times do |i|
+			if littles.include? res[i]
+				x += ' '+res[i]
+			else
+				x += ' '+res[i].capitalize
+			end
+		end
+
+		@title = x[1].upcase + x[2..-1]
+		p @title
+	end
+
+	def title
+		@title
+	end
 end
